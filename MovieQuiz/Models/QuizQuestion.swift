a7a185d0273fcaf22c6845f573ec9ffb56bd80d1
irinadeeva <51794769+irinadeeva@users.noreply.github.com>
2023-10-02 17:27:02 +0200

    isolated models implemented Factory, Delegate and DI patterns

---
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,14 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Irina Deeva on 28/09/23.
+//
+
+import Foundation
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Irina Deeva on 28/09/23.
+//
+
+import Foundation
+
+protocol QuestionFactoryProtocol {
+    func requestNextQuestion()
+}
