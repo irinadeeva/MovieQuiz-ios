6c33fe22b8e3fcebdda841a7dd39027aa3c7e32f
irinadeeva <51794769+irinadeeva@users.noreply.github.com>
2023-10-29 18:36:29 +0100

    created a GET request to  API IMDb

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(hanlder: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+
+struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://imdb-api.com/API/Top250Movies/k_zcuw1ytf") else {
+            preconditionFailure("Unable to construct mostPopilarMoviesURL")
+        }
+        return url
+    }
+    
+    func loadMovies(hanlder: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    hanlder(.success(mostPopularMovies))
+                } catch {
+                    hanlder(.failure(error))
+                }
+            case .failure(let error):
+                hanlder(.failure(error))
+            }
+        }
+    }
+}
